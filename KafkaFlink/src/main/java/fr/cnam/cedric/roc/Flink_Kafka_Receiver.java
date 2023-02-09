24de0e5e2bb13c9d30e8aa46be5f512345404ce9
Patient Ntumba Wa Ntumba <pntumba@pl1162-pro.paris.inria.fr>
2023-02-09 18:11:48 +0100

    x

--- KafkaFlink/src/main/java/fr/cnam/cedric/roc/Flink_Kafka_Receiver.java
+++ KafkaFlink/src/main/java/fr/cnam/cedric/roc/Flink_Kafka_Receiver.java
@@ -2,16 +2,16 @@ package fr.cnam.cedric.roc;
 
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import java.util.Properties;
 
 public class Flink_Kafka_Receiver {
 
     public static void main(String[] args) throws  Exception{
 
-        String inputTopic="flink-group";
+        String inputTopic="testtopic";
         String server="localhost:9092";
         StramConsumrer(inputTopic,server);
     }
@@ -19,7 +19,7 @@ public class Flink_Kafka_Receiver {
     public static void StramConsumrer(String inputTopic, String server) throws Exception{
 
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
-        FlinkKafkaConsumer<String> flinkKafkaConsumer = createStringConsumerForTopic(inputTopic, server);
+        FlinkKafkaConsumer011<String> flinkKafkaConsumer = createStringConsumerForTopic(inputTopic, server);
         DataStream<String> stringInputStream = environment.addSource(flinkKafkaConsumer);
         stringInputStream.map(new MapFunction<String, String>() {
             @Override
@@ -30,12 +30,12 @@ public class Flink_Kafka_Receiver {
         environment.execute();
     }
 
-    private static FlinkKafkaConsumer<String> createStringConsumerForTopic(String inputTopic, String server) {
+    private static FlinkKafkaConsumer011<String> createStringConsumerForTopic(String inputTopic, String server) {
 
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", server);
         //props.setProperty("group.id",kafkaGroup);
-        FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>(inputTopic, new SimpleStringSchema(), properties);
+        FlinkKafkaConsumer011<String> consumer = new FlinkKafkaConsumer011<>(inputTopic, new SimpleStringSchema(), properties);
         return consumer;
     }
 }
